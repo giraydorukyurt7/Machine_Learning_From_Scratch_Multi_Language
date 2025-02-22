4f27bb17b0960ba7002df0e5ec22e3aa44a22ed1
Giray Doruk Yurtseven <giraysoftwareengineer3.14@hotmail.com>
2025-02-22 23:55:05 +0300

    Simple Linear Regression - Python 2

--- Python/1_Simple_Linear_Regression/simple_linear_regression.py
+++ Python/1_Simple_Linear_Regression/simple_linear_regression.py
@@ -30,6 +30,11 @@ for i in range(len(dataset)):
     X.append(dataset[i][0])
 for i in range(len(dataset)):
     Y.append(dataset[i][1])
+#Turn into integer float lists
+X = [float(x) for x in X]
+Y = [float(y) for y in Y]
+
+
 
 def test_train_split(X,Y,train_percentage):
     0.8
@@ -57,7 +62,51 @@ print(f"Length of y_train: {len(y_train)}")
 print(f"Length of X_test: {len(X_test)}")
 print(f"Length of y_test: {len(y_test)}")
 
-print(X_train)
-print(y_train)
-print(X_test)
-print(y_test)
+#print(X_train)
+#print(y_train)
+#print(X_test)
+#print(y_test)
+
+# y= mx+b
+
+def find_m(X,Y):
+    n = len(X)
+    #sigma_xi_times_yi = 0
+    #for i in range(n): #0,n-1
+    #    xi_times_yi = X[i]*Y[i]
+    #    sigma_xi_times_yi+=xi_times_yi
+    
+    #Find mean_X and mean_Y
+    total_X = 0
+    total_Y = 0
+    for x in X:
+        total_X+=x
+    for y in Y:
+        total_Y+=y
+    mean_X = total_X/len(X)
+    mean_Y = total_Y/len(Y)
+
+    sigma_xi_minus_mean_X__times__yi_minus_mean_Y = 0
+    for i in range(n): #0,n-1
+        sigma_xi_minus_mean_X__times__yi_minus_mean_Y = (X[i]-mean_X)*(Y[i]-mean_Y)
+    sigma_xi_minus_mean_X_squared = 0
+    for i in range(n):
+        sigma_xi_minus_mean_X = (X[i]*mean_X)**2
+    
+    m = sigma_xi_minus_mean_X__times__yi_minus_mean_Y /sigma_xi_minus_mean_X
+
+    b = mean_Y-m*mean_X
+
+    return m,b
+#print(type(X_train))
+#print(type(y_train))
+#
+#print(type(X_train[5]))
+
+#print(any(not isinstance(x, (int, float)) for x in X_train))  # Check for non-numeric types
+#print(any(not isinstance(y, (int, float)) for y in y_train))
+
+m,b = find_m(X=X_train, Y=y_train)
+
+print(m)
+print(b)
